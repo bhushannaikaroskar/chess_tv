5a93a731e2f77ef96c8dd756b00ff360159a91ee
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-05-15 01:06:01 +0530

    minor change

--- src/context/index.js
+++ src/context/index.js
@@ -2,7 +2,9 @@
 
 import AuthProvider from "./AuthProvider";
 import VideoProvider from "./VideoProvider";
+import LikeProvider from "./LikeProvider";
 export {useAuth} from "./AuthProvider"
 export {useVideos} from "./VideoProvider"
+export {useLikes} from "./LikeProvider"
 
-export {AuthProvider,VideoProvider}
+export {AuthProvider,VideoProvider,LikeProvider}
