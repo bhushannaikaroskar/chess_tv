f1e350848365673a0b5b9fa112bb8c0e98998bde
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-05-19 18:15:41 +0530

    add: auth token to local storage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { createContext, useContext, useReducer, useState } from "react";
+import {
+    createContext,
+    useContext,
+    useEffect,
+    useReducer,
+    useState,
+} from "react";
 import { errorToast, successToast } from "../utils";
 import { useTheme } from "./ThemeProvider";
 
@@ -16,7 +22,7 @@ const initialAuthState = {
 };
 
 export default function AuthProvider({ children }) {
-    let email,firstName,lastName,_id;
+    let email, firstName, lastName, _id;
     const authReducer = (state, action) => {
         switch (action.type) {
             case "VERIFIED":
@@ -29,13 +35,13 @@ export default function AuthProvider({ children }) {
                     authToken: action.payload.token,
                 };
             case "SIGNUP_VERIFIED":
-                ({email,firstName,lastName,_id} = action.payload.createdUser);
+                ({ email, firstName, lastName, _id } =
+                    action.payload.createdUser);
                 return {
                     ...state,
                     isAuthenticated: true,
-                    user:{email,firstName,lastName,_id},
+                    user: { email, firstName, lastName, _id },
                     authToken: action.payload.token,
-
                 };
             case "RESET":
                 return { ...initialAuthState };
@@ -45,8 +51,8 @@ export default function AuthProvider({ children }) {
     };
 
     const [auth, dispatchAuth] = useReducer(authReducer, initialAuthState);
-    const [error,setError] = useState();
-    const {theme} = useTheme()
+    const [error, setError] = useState();
+    const { theme } = useTheme();
 
     const loginUser = (email, password) => {
         axios
@@ -64,13 +70,13 @@ export default function AuthProvider({ children }) {
                     type: "VERIFIED",
                     payload: { token: res.data.encodedToken, ...res.data },
                 });
-                successToast("Logged In",theme)
-                setError("")
+                successToast("Logged In", theme);
+                setError("");
             })
             .catch((err) => {
                 dispatchAuth({ type: "RESET" });
-                errorToast("There was some error while logging in",theme)
-                setError(err.response.data.errors[0])
+                errorToast("There was some error while logging in", theme);
+                setError(err.response.data.errors[0]);
             });
     };
 
@@ -87,25 +93,48 @@ export default function AuthProvider({ children }) {
             .then((res) => {
                 dispatchAuth({
                     type: "SIGNUP_VERIFIED",
-                    payload: { token: res.data.encodedToken,...res.data },
+                    payload: { token: res.data.encodedToken, ...res.data },
                 });
-                successToast("Signin Successful",theme)
-                setError("")
+                successToast("Signin Successful", theme);
+                setError("");
             })
             .catch((err) => {
                 dispatchAuth({ type: "RESET" });
-                errorToast("There was some error in signing",theme)
-                setError(err.response.data.errors[0])
+                errorToast("There was some error in signing", theme);
+                setError(err.response.data.errors[0]);
             });
     };
 
     const logout = () => {
         dispatchAuth({ type: "RESET" });
-        successToast("Successfully logged out",theme)
+        successToast("Successfully logged out", theme);
     };
 
+    useEffect(()=>{
+        if(auth.isAuthenticated){
+            localStorage.setItem("chess-tv-user-auth",JSON.stringify(auth))
+        }
+    },[auth])
+
+    useEffect(() => {
+        if (localStorage.getItem("chess-tv-user-auth")) {
+            const storedState = JSON.parse(
+                localStorage.getItem("chess-tv-user-auth")
+            );
+
+            if (storedState.authToken) {
+                dispatchAuth({
+                    type: "VERIFIED",
+                    payload: { token: storedState.authToken, foundUser:storedState.user },
+                });
+            }
+        }
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ auth, loginUser, signUpUser, logout, error }}>
+        <AuthContext.Provider
+            value={{ auth, loginUser, signUpUser, logout, error }}
+        >
             {children}
         </AuthContext.Provider>
     );
