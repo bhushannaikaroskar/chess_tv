89e182c3a0a69520e36b08758b3890795fb90359
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2022-06-23 01:11:52 +0530

    style: add active link css for sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,31 +1,62 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./sidebar.css";
+
+const navigations = [
+    {
+        name:"Home",
+        pathname:"/",
+        icon:"home",
+        isOutlined:true
+    },
+    {
+        name:"Explore",
+        pathname:"/explore",
+        icon:"explore",
+        isOutlined:true
+    },
+    {
+        name:"Playlist",
+        pathname:"/playlist",
+        icon:"playlist_play",
+        isOutlined:false
+    },
+    {
+        name:"Liked Videos",
+        pathname:"/liked_videos",
+        icon:"thumb_up",
+        isOutlined:true
+    },
+    {
+        name:"Watch Later",
+        pathname:"/watch_later",
+        icon:"schedule",
+        isOutlined:false
+    },
+    {
+        name:"History",
+        pathname:"/history",
+        icon:"history",
+        isOutlined:false
+    },
+]
 
 export default function Sidebar() {
 
     const navigate = useNavigate()
+    const location = useLocation();
 
     return (
         <aside className="grand-filter sidebar sidebar-hidden p-y-2">
-            <button className="btn-nav" onClick={()=>{navigate('/')}}>
-                <span className="material-icons-outlined">home</span><div className="btn-nav-text">Home</div>
-            </button>
-            <button className="btn-nav" onClick={()=>{navigate('/explore')}}>
-                <span className="material-icons-outlined">explore</span><div className="btn-nav-text">Explore</div>
-            </button>
-            <button className="btn-nav" onClick={()=>{navigate('/playlist')}}>
-                <span className="material-icons">playlist_play</span><div className="btn-nav-text">Playlist</div>
-            </button>
-            <button className="btn-nav" onClick={()=>{navigate('/liked_videos')}}>
-                <span className="material-icons-outlined">thumb_up</span> <div className="btn-nav-text">Liked Videos</div>
-            </button>
-            <button className="btn-nav" onClick={()=>{navigate('/watch_later')}}>
-                <span className="material-icons">schedule</span> <div className="btn-nav-text">Watch Later</div>
-            </button>
-            <button className="btn-nav" onClick={()=>{navigate('/history')}}>
-                <span className="material-icons">history</span><div className="btn-nav-text">History</div>
-            </button>
+        {navigations.map((nav)=>{
+            const isActive = location.pathname === nav.pathname || (location.pathname.includes("/video") && nav.pathname === "/explore")?"btn-nav-active":""
+            return (
+                <button className={`btn-nav ${isActive}`} onClick={()=>{navigate(nav.pathname)}}>
+                    <span className={`material-icons${nav.isOutlined?"-outlined":""}`}>{nav.icon}</span>
+                    <div className="btn-nav-text">{nav.name}</div>
+                </button>
+            )
+        })} 
         </aside>
     );
 }
